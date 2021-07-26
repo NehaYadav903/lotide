b01de17597fcbcde9fd5f3e5fe5231b634dd9bd3
Neha Yadav <nehayadav903@gmail.com>
2021-07-26 16:18:26 -0400

    function tail which returns the tail of an array

---
+++ tail.js
@@ -0,0 +1,15 @@
+
+const tail = function(arr) {
+  return arr.slice(1);
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else
+  {
+    console.log("🛑🛑🛑Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+  }
+};
+  // TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]);
