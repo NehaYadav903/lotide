3ab4a92c83fdcf441ee8616c8b5034f6d604fd42
Neha Yadav <nehayadav903@gmail.com>
2021-07-30 12:22:43 -0400

    return results indicating that L appears twice, and H once

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(texts) {
+  let result = {};
+  for (const text of texts) {
+    if (result[text] === undefined) {
+      result[text] = 1;
+    } else {
+      result[text] += 1;
+    }
+    
+  }
+  return result;
+};
+console.log(countLetters('LHL'));
