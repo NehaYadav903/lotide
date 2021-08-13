06d74b967d39d3498a92cf732f07e2dc25a9b488
Neha Yadav <nehayadav903@gmail.com>
2021-08-13 16:10:28 -0400

    update

--- eqArrays.js
+++ eqArrays.js
@@ -1,29 +1,10 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(arrayOne,arrayTwo){
- let result = true;
- for(let x = 0; x< arrayOne.length; x++){
-  if(arrayOne[x] !== arrayTwo[x]){
+const eqArrays = function(array1,array2){
+ for(let x = 0; x< array1.length; x++){
+  if(array1[x] !== array2[x])
    result = false;
    }
+   return true;
   }
-if(result === true){
- console.log("Assertion Passed:" + arrayOne + "===" + arrayTwo);
- }else{
- console.log("Assertion Failed:"  + arrayOne + "!==" + arrayTwo);
-}
-return result;
-};
-
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🛑🛑🛑Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
-  }
-};
+  module.exports = eqArrays;
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
-eqArrays([1, 2, 3], [3, 2, 1]) ;
