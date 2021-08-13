86ce1925285bc839dd385f4f2549a2df6a3406bc
Neha Yadav <nehayadav903@gmail.com>
2021-08-13 16:35:53 -0400

    Update the code

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (var x = 0; x < arr1.length; x++) {
+      if (arr1[x] !== arr2[x]) {
+        console.log("Arrays are not the same.");
+        return false;
+      }
+    }
+  } else {
+    console.log("Arrays are not the same length.");
+    return false;
+  }
+console.log("Arrays are the same.");
+return true;
+}
+
+
+module.exports = assertArraysEqual;
