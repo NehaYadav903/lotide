f25d016681b8b36b06d2d0a4e37b78a5b80d1a2a
Neha Yadav <nehayadav903@gmail.com>
2021-08-13 14:56:50 -0400

    Update

--- tail.js
+++ tail.js
@@ -1,7 +1,4 @@
 
-const tail = function(arr) {
-  return arr.slice(1);
-}
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
@@ -10,6 +7,14 @@ const assertEqual = function(actual, expected) {
     console.log("🛑🛑🛑Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
   }
 };
+const tail = function(arr) {
+  return arr.slice(1);
+}
   // TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+assertEqual(result.length,2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+const words = ["Yo Yo", "Lighthouse" , "Labs"];
+tail(words);
+assertEqual(words.length, 3);
