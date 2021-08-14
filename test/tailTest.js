ddfca721056ca30df047792edd9866a946a4aa7b
Neha Yadav <nehayadav903@gmail.com>
2021-08-13 20:32:26 -0400

    Update

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,14 @@
-const assertEqual = require('../assertEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail = require('../tail');
+
+
+// assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length,2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-const words = ["Yo Yo", "Lighthouse" , "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
