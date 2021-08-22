5149d181289f6b0bddaaa61343c1eb3814e9451f
Neha Yadav <nehayadav903@gmail.com>
2021-08-22 02:57:00 -0400

    Refactored the code

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,20 @@
-const eqArrays = function(array1,array2){
- for(let x = 0; x< array1.length; x++){
-  if(array1[x] !== array2[x])
-   result = false;
-   }
-   return true;
-  }
-  module.exports = eqArrays;
+//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
+const assertEqual = require('./assertEqual');
+const eqArrays = function(array1, array2) { //function that can compare two arrays for a perfect match.
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); 
+module.exports = eqArrays;
 
