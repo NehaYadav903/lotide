a934aa0b3d8bafca061fcfedfa24b8d9bfecb2fb
Neha Yadav <nehayadav903@gmail.com>
2021-08-23 19:10:27 -0400

    Update the code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;	
+describe("#assertEqual", () => {
+  it("returns true if array are equal", () => {
+  assert.strictEqual(1, 1);
+	});
+	it("return false if array are not equal",() => {
+	assert.strictEqual('lighthouse Labs', 'bootcamp'); 
+	});
+	});
+  
+	// assertEqual("Lighthouse Labs", "Bootcamp");
+	// assertEqual(1, 1);
