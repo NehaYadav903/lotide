4fd9b0f4757a3c8b9ed4b630fe35292596d85a36
Neha Yadav <nehayadav903@gmail.com>
2021-08-23 19:21:02 -0400

    Update the code

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+//   } else {
+//     console.log("🛑🛑🛑Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+//   }
+// };
+const assertEqual = require('./assertEqual');
+const findKeyByValue = function(obj, val) {
+  for (let key of Object.keys(bestTVShowsByGenre)) { 
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), "adventure");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
