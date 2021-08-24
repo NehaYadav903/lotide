e1ec4345b6e340e2f181487870e7ba4069ed4da8
Neha Yadav <nehayadav903@gmail.com>
2021-08-23 23:42:27 -0400

    Update the code

---
+++ takeUntil.js
@@ -0,0 +1,48 @@
+// const assertArraysEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
+//   }
+// };
+// const eqArrays = function(array1, array2) { //function that can compare two arrays for a perfect match.
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+// FUNCTION IMPLEMENTATION
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (const el of array) {
+    if (callback(el) === true) {
+      return results;
+    }
+    results.push(el);
+  }
+  return results;
+};
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); 
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+  
+//expected output
+// [ 1, 2, 5, 7, 2 ]
+// ---
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]  
+  
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]); 
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
+//passed
