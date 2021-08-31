604a1d6f5de50f07bf26b972057b8dd47fdb2e6a
Neha Yadav <nehayadav903@gmail.com>
2021-08-31 17:02:17 -0400

    Add module.exports to improve functionality

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,11 @@
 
 const assertEqual = require('./assertEqual');
+
 // FUNCTION IMPLEMENTATION
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    /*inside the loop, 
     //increment the counter for each item:
-    //set a property with that string key to:
     //the value that was already there (or zero if nothing there) with 1 added to it.*/
     if (itemsToCount[item]) {
       if (results[item]) {
@@ -35,3 +34,5 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+module.exports = countOnly;
