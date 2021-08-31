c7d9dad4f137f42dab6a0d5c4c2ed41e1d21114b
Neha Yadav <nehayadav903@gmail.com>
2021-08-31 17:05:51 -0400

    Remove console.log for clear code

--- flatten.js
+++ flatten.js
@@ -1,23 +1,6 @@
-// const assertArraysEqual = function(actual, expected) {
-//   if (eqArrays(actual, expected)) {
-//     console.log(`✅✅✅ Assertion Passed:${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`);
-//   }
-// };
 const assertEqual = require('./assertEqual');
-// const eqArrays = function(array1, array2) { //function that can compare two arrays for a perfect match.
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
 const eqArrays = require('./eqArrays');
+
 //FUNCTION IMPLEMENTATION
 const flatten = function(array1) {
   let flattened = [];
@@ -32,5 +15,7 @@ const flatten = function(array1) {
   }
   return flattened;
 };
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([1, 2, [2, 3, 6, 3, 4], 8, 0, 0, 8, 6]));
+flatten([1, 2, [3, 4], 5, [6]]);
+flatten([1, 2, [2, 3, 6, 3, 4], 8, 0, 0, 8, 6]);
+
+module.exports = flatten;
