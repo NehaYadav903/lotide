082f7d2e28cf883c042aa398a7588938b997452f
Neha Yadav <nehayadav903@gmail.com>
2021-08-31 16:57:15 -0400

    Remove unwanted comments to make code more readable

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 module.exports = assertEqual;
-// Test for assertEqual
-// assertEqual("Lighthouse Labs", "Bootcamp");
-//  assertEqual(1, 1);
+
 	
